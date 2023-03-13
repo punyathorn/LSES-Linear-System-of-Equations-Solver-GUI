9ff86ea78d6b279a5a3a7cf27f1bd24c1d1bab13
Punyathorn <93460088+punyathorn@users.noreply.github.com>
2023-03-13 19:44:03 +0700

    Add files via upload

--- Matrix.py
+++ Matrix.py
@@ -1,29 +1,3 @@
-"""
-
-MIT License
-
-Copyright (c) 2021-present Punyathorn
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-"""
-
 from tkinter import *
 import tkinter
 from tkinter import ttk, messagebox
@@ -164,16 +138,10 @@ def get():
             yeq = (-x)/y*i + equal_result/y
             xs.append(i)
             ys.append(yeq)
-            print(i, yeq) 
-            print("")
-            print(xs, ys)
         for i in range (int(-10), int(11)):
             yeq2 = (-x2)/y2*i + equal_result2/y2
             xs2.append(i)
             ys2.append(yeq2)
-            print(i, yeq2) 
-            print("")
-            print(xs2, ys2)
         plt.plot(xs,ys,label=eq)
         plt.plot(xs2,ys2,label=eq2)
         plt.title('No Solution (parallel lines)')
@@ -201,16 +169,10 @@ def get():
             yeq = (-x)/y*i + equal_result/y
             xs.append(i)
             ys.append(yeq)
-            print(i, yeq) 
-            print("")
-            print(xs, ys)
         for i in range (int(-10+round(result_x)), int(11+round(result_x))):
             yeq2 = (-x2)/y2*i + equal_result2/y2
             xs2.append(i)
             ys2.append(yeq2)
-            print(i, yeq2) 
-            print("")
-            print(xs2, ys2)
         plt.plot(xs,ys,label=eq)
         plt.plot(xs2,ys2,label=eq2)
         plt.scatter(result_x,result_y,label=(result_x,result_y))
